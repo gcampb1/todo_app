d2f6b0037c916307ae0e068684691fb76ab4a70c
Garrett D. Campbell <gcampb1@gmail.com>
2018-01-15 21:05:33 -0600

    defined index, new, show, and edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+
+  end
+
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,17 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-
+describe 'GET #index' do
   it 'renders the index template' do
     get :index
 
     expect(response).to render_template(:index)
   end
 
-  it 'renders the show template'
+  it 'returns all the tasks for the user'do
+      user =create(:user)
 
-  it 'renders the edit template'
+      get :index
 
-  it 'renders the new template'
+      expect(assigns(:tasks)).not_to be_nil
+    end
+  end
+
+  describe 'GET #new' do
+  it 'renders the new template' do
+    get :new
+
+    expect(response).to render_template(:new)
+    end
+
+    it 'assigns a new task to @task' do
+      get :new
+
+      expect(assigns(:task)).to be_a_new(Task)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'renders the show template' do
+      task = create(:homework)
+
+      get :show, params: { id: task.to_param }
+
+      expect(response).to render_template(:show)
+    end
+
+    it 'assigns the requested task as @task' do
+      task = create(:homework)
+
+      get :show, params: { id: task.to_param }
+
+     # raise task.inspect
+
+      expect(assigns(:task)).to eq(task)
+    end
+  end
+
+  describe 'GET #edit' do
+  it 'renders the edit template' do
+    task = create(:email)
+    get :edit, params: { id: task.to_param}
+
+    expect(response).to render_template(:edit)
+    end
+
+    it 'assigns the requested task as @task' do
+      task = create(:homework)
+      get :edit, params: { id: task.to_param }
+
+      expect(assigns(:task)).to eq(task)
+    end
+  end
 
 end
